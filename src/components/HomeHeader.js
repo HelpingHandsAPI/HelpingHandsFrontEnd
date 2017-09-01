f85365d312e803ea1d73140feec3a021996a3801
Robby Dore <rdore88@gmail.com>
2017-09-01 13:09:35 -0400

    Created basic layout for home and added components needed for home

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class HomeHeader extends Component {
+  render(){
+    return(
+      <div className="home-header">
+        <h1>Welcome to the Helping Hands Website</h1>
+        <h3>An all in one resource to help the needy</h3>
+      </div>
+    )
+  }
+}
---
+++ src/components/ResourceIcons.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class ResourceIcons extends Component {
+  render(){
+    let match = this.props.match;
+    return(
+      <h1>Resource Icons</h1>
+    )
+  }
+}
